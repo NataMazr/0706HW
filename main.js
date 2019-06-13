897b1af09102dd816306be094ffcce49f373a570
Nata <generalmnf@gmail.com>
2019-06-13 08:44:54 +0300

    не выходит за границы

--- main.js
+++ main.js
@@ -1,48 +1,28 @@
-// let m = document.querySelector('.main');
-// const msrc = document.querySelector('.main').src;
-// let img = document.querySelectorAll('.c');
-// for (let i = 0; i < img.length; i++) {
-//     img[i].addEventListener('mouseover', function() {
-//         m.src = img[i].src;
-//     })
-//     img[i].addEventListener('mouseout', function() {
-//         m.src = msrc;
-//     })
-
-// }
-
-function move(div) {
-    document.onmousedown = function() { return false }
-    div.style.cursor = 'move';
-    document.onmousemove = function(e) {
-        x = e.pageX;
-        y = e.pageY;
-        left1 = div.offsetLeft;
-        top1 = div.offsetTop;
-        left1 = x - left1;
-        top1 = y - top1;
-        document.onmousemove = function(e) {
-            x = e.pageX;
-            y = e.pageY;
-            div.style.top = y - top1 + 'px';
-            div.style.left = x - left1 + 'px';
-        }
+let circ = document.querySelector('.circ');
+circ.addEventListener('mousedown', function() {
+    circ.addEventListener('mousemove', ev)
+})
+let ev = function(e) {
+    let x = e.pageX;
+    let y = e.pageY;
+    console.log(x, y);
+    if (x > 711) {
+        x = 711;
+    } else if (x < 112) {
+        x = 112;
+    } else {
+        x = x;
     }
-    document.onmouseup = function() {
-        div.style.cursor = 'auto';
-        document.onmousedown = function() {}
-        document.onmousemove = function() {}
+    if (y > 460) {
+        y = 460;
+    } else if (y < 112) {
+        y = 112;
+    } else {
+        y = y;
     }
-}
-
-
-//     div.style.left = e.pageX + "px";
-//     div.style.top = e.pageY + "px";
-// };
-// function move() {
-//     document.addEventListener('onMouseUp', listener);
-// }
-
-// function stop() {
-//     document.removeEventListener('MouseDown', listener);
-//
+    circ.style.top = y - 100 + 'px';
+    circ.style.left = x - 100 + 'px';
+};
+circ.addEventListener('mouseup', function() {
+    circ.removeEventListener('mousemove', ev)
+})
